ec6290da8b9fe553457c12ca4e2cd3b81e292ab6
mohammadamin <amin.sousaniii@gmail.com>
2023-04-14 03:41:54 -0700

    aa

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -10,23 +10,24 @@ import { Container } from 'react-bootstrap'
            <Container>
             <div className='about'>
               <div className='card1'>
-                <p>aa</p>
-                <p>fff</p>
-                
-                <p></p>
-                <p></p>
+                <h2>personal info</h2>
+                <div>fullname  :<strong className='strong'>amin-sousani</strong> </div>
+                <div>age:<strong className='strong'>20</strong> </div>
+                <div>locitoin :<strong className='strong'>karmansh</strong> </div>
+                <div> student:  <strong className='strong'>computer</strong></div>
               </div>
               <div className='card2'>
-                <p>aa</p>
-                <p>fff</p>
-               
-                <p></p>
-                <p></p>
+                <h2>skill</h2>
+                <div>html %100</div>
+                <div>css %60</div>
+                <div>react %20</div>
+                <div>sass %70</div>
+                <div>js %40</div>
               </div>
             </div>
 
             <div className='about'>
-              amin
+              
             </div>
             </Container>
            <Footer></Footer>
